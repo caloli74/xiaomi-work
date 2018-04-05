acf36066497d6a852bf6616f53c751edba2b0d47
caloli74 <caloli1974@gmail.com>
2018-04-05 11:59:27 +0200

    dev

---
+++ app.js
@@ -0,0 +1,14 @@
+const Xiaomi = require('./lib/Xiaomi.js');
+
+var xiaomi = new Xiaomi('wkj8q0gi2nmaugth');
+xiaomi.on('ready', () => {
+    //xiaomi.playSound(12);
+    //xiaomi.setColor({intensity:20, r:0, g:255, b:0});
+    console.log('xiaomi ready');
+});
+
+xiaomi.on('message', (msg) => {
+    //xiaomi.playSound(12);
+    //xiaomi.setColor({intensity:20, r:0, g:255, b:0});
+    console.log(JSON.stringify(msg));
+});
